a1feb3e97f945d4119789a7bcd0ac33907700929
Laurence <faroe228@gmail.com>
2013-12-12 15:11:13 -0800

    lpt - minor changes to make work on windows

--- cpm.bat
+++ cpm.bat
@@ -1,3 +1,6 @@
-set CPM_PATH=.
+@rem lpt minor changes to make work on windows
+@pushd "%~dp0"
+set CPM_PATH=%CD%
+@popd
 set CPM_REGISTRY=http://packages.dojofoundation.org/
-java -classpath %CPM_PATH%\jars\js.jar org.mozilla.javascript.tools.shell.Main -opt -1 %CPM_PATH%\lib\cpm.js %*
+java -classpath "%CPM_PATH%\jars\js.jar" org.mozilla.javascript.tools.shell.Main -opt -1 "%CPM_PATH%\lib\cpm.js" %*
