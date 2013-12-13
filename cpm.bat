7f6f228788576ccd0e0f59098db6f154e1db52ac
Laurence <faroe228@gmail.com>
2013-12-13 14:33:16 -0800

    lpt - cpm install dojox fix

--- cpm.bat
+++ cpm.bat
@@ -3,4 +3,8 @@
 set CPM_PATH=%CD%
 @popd
 set CPM_REGISTRY=http://packages.dojofoundation.org/
-java -classpath "%CPM_PATH%\jars\js.jar" org.mozilla.javascript.tools.shell.Main -opt -1 "%CPM_PATH%\lib\cpm.js" %*
+@rem cpm install dojox FAILS without my fix for cpm.js
+@rem https://github.com/kriszyp/cpm/issues/20
+@java -classpath "%CPM_PATH%\jars\js.jar" org.mozilla.javascript.tools.shell.Main -opt -1 "%CPM_PATH%\lib\cpm.js" %*
+@rem switching to node is another solution to cpm install dojox issue
+@rem node -opt -1 "%CPM_PATH%\lib\cpm.js" %*
